0544e79a6b21b39500a65d30f4ed185534b28178
PuddingPudding <hwsh70119@gmail.com>
2017-02-28 18:16:59 +0800

    寫好sin函式，將弧度帶入，便可獲得其sin值

--- src/MySin.java
+++ src/MySin.java
@@ -1,10 +1,23 @@
 
 public class MySin 
 {
+	public static double sin(double input)
+	{
+		double output = input;
+		int change = -1;  //用來控制每次的變因，第一次是減，第二次是加，以此類推
+		for(int i = 1 ; i <= 10 ; i++)
+		{
+			output += change * (Math.pow(input, i*2+1) / factorial(i*2+1) );
+			change *= -1;
+		}
+		return output;
+	}
+	
+	
 	/*
 	 * 數學的階乘; factorial(3) = 3*2*1
 	 */
-	double factorial(double s) 
+	public static double factorial(double s) 
 	{
 		double r = 1;
 
